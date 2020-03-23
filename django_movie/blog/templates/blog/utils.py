2bfa356ee85b89a134b92e607d5f3e468f146730
Andrey <andrey_zhuk0.1@mail.ru>
2020-03-23 22:00:59 +0500

    syntaxError?

---
+++ django_movie/blog/templates/blog/utils.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import *
+
+class ObjectDetailMixin:
+    model = None
+    template = None
+
+
+    def get(self, reguest, slug):
+        obj = get_object_or_404(self.model, slug__iexact=slug)
+        return render(reguest, self.template, context={"post": post})
+
+
+
--- django_movie/blog/views.py
+++ django_movie/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
 from .models import Post, Tag
 # Create your views here.
 
@@ -8,9 +9,16 @@ def posts_list(reguest):
     return render(reguest, "blog/index.html", context={"posts": posts})
 
 
-def post_detail(reguest, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(reguest, "blog/post_detail.html", context={"post": post})
+class PostDetail(View):
+    def get(self, reguest, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(reguest, "blog/post_detail.html", context={"post": post})
+
+
+class TagDetail(View):
+    def get(self, reguest, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(reguest, "blog/tag_detail.html", context={"tag": tag})
 
 
 def tags_list(reguest):
@@ -18,6 +26,5 @@ def tags_list(reguest):
     return render(reguest, "blog/tags_list.html", context={"tags": tags})
 
 
-def tag_detail(reguest, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(reguest, "blog/tag_detail.html", context={"tag": tag})
+
+
