a7f73cfef177ae184d1f15fc0ad1c8e6ef24018f
Andrey <andrey_zhuk0.1@mail.ru>
2020-03-23 19:46:40 +0500

    new table 'Tags'

--- django_movie/blog/views.py
+++ django_movie/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, Tag
 # Create your views here.
 
 def posts_list(reguest):
@@ -11,3 +11,13 @@ def posts_list(reguest):
 def post_detail(reguest, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(reguest, "blog/post_detail.html", context={"post": post})
+
+
+def tags_list(reguest):
+    tags = Tag.objects.all()
+    return render(reguest, "blog/tags_list.html", context={"tags": tags})
+
+
+def tag_detail(reguest, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(reguest, "blog/tag_detail.html", context={"tag": tag})
