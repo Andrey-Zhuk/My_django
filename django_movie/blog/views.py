bd82fd76b5337c37a6d2ccc59cd3b03adb84c2f1
Andrey <andrey_zhuk0.1@mail.ru>
2020-03-26 10:47:06 +0500

    new form

--- django_movie/blog/views.py
+++ django_movie/blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 from .models import Post, Tag
-# Create your views here.
+
 from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 
+# Create your views here.
 def posts_list(reguest):
     posts = Post.objects.all()
     return render(reguest, "blog/index.html", context={"posts": posts})
@@ -25,6 +28,20 @@ class TagDetail(ObjectDetailMixin, View):
     # def get(self, reguest, slug):
     #     tag = get_object_or_404(Tag, slug__iexact=slug)
     #     return render(reguest, "blog/tag_detail.html", context={"tag": tag})
+
+class TagCreate(View):
+    def get(self, reguest):
+        form = TagForm()
+        return render(reguest, "blog/tag_create.html", context={"form": form})
+
+    def post(self, reguest):
+        bound_form = TagForm(reguest.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(reguest, "blog/tag_create.html", context={"form": bound_form})
+
 
 
 def tags_list(reguest):
