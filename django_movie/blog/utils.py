6c66d0bad56a3651084067a0178aa0ca7646c394
Andrey <andrey_zhuk0.1@mail.ru>
2020-03-28 12:26:36 +0500

    delete tag and post

--- django_movie/blog/utils.py
+++ django_movie/blog/utils.py
@@ -50,3 +50,17 @@ class ObjectUpdateMixin:
             return redirect(new_obj)
         return render(reguest, self.template, context={"form": bound_form, self.model.__name__.lower(): obj})
 
+
+class ObjectDeleteMixin:
+    model = None
+    template = None
+    redirect_url = None
+
+    def get(self, reguest, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        return render(reguest, self.template, context={self.model.__name__.lower(): obj})
+
+    def post(self, reguest, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        obj.delete()
+        return redirect(reverse(self.redirect_url))
--- django_movie/blog/views.py
+++ django_movie/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 from .models import Post, Tag
@@ -27,6 +28,11 @@ class PostUpdate(ObjectUpdateMixin, View):
     model_form = PostForm
     template = "blog/post_update_form.html"
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = "blog/post_delete_form.html"
+    redirect_url = "posts_list_url"
+
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
@@ -43,20 +49,11 @@ class TagUpdate(ObjectUpdateMixin, View):
     model_form = TagForm
     template = "blog/tag_update_form.html"
 
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = "blog/tag_delete_form.html"
+    redirect_url = "tags_list_url"
 
-    # def get(self, reguest, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(instance=tag)
-    #     return render(reguest, "blog/tag_update_form.html", context={"form": bound_form, "tag": tag})
-    #
-    # def post(self, reguest, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(reguest.POST, instance=tag)
-    #
-    #     if bound_form.is_valid():
-    #         new_tag = bound_form.save()
-    #         return redirect(new_tag)
-    #     return render(reguest, "blog/tag_update_form", context={"form": bound_form, "tag": tag})
 
 
 def tags_list(reguest):
